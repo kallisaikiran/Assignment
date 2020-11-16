736b40cbd4e5a483820aa6a427e6f7e5d734aba6
kallisaikiran <kalli.saikiran@outlook.com>
2020-11-16 12:44:34 -0500

    taken array elements in mainactivity java file to show five movie names and it ratings, description with images

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -41,9 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MovieCustomAdapter adapter=new MovieCustomAdapter(this, maintitle, rating,imgid,description);
-        list=(ListView)findViewById(R.id.list);
-        list.setAdapter(adapter);
+        
 
 
 
