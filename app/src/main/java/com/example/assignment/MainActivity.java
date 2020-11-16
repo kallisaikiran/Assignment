d334a0dfa19a244636b034eecf6df7566e22cda0
kallisaikiran <kalli.saikiran@outlook.com>
2020-11-16 12:43:57 -0500

    taken array elements in mainactivity java file to show five movie names and it ratings, description with images

---
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.assignment;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.assignment.adaptors.MovieCustomAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    ListView list;
+
+    String[] maintitle ={
+            "UnnadiOkateyJindhagi","Linga","Narappa","Devadas","Dhrushyam",
+    };
+
+    String[] rating ={
+            "Rating:3/5","Rating:2/5",
+            "Rating:4/5","Rating:4.5/5",
+            "Rating:5/5",
+    };
+
+
+    String[] description ={
+            "this movie about Love story between two persons","This movie about bank robbery by two Brains",
+            "plan to save his son from crucial situation","A beautiful gang war love story between two friends",
+            "plan to escape his daughter from murder situations",
+    };
+
+    Integer[] imgid={
+            R.drawable.download_1,R.drawable.download_2,
+            R.drawable.download_3,R.drawable.download_4,
+            R.drawable.download_5,
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MovieCustomAdapter adapter=new MovieCustomAdapter(this, maintitle, rating,imgid,description);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+
+
+    }
+}
